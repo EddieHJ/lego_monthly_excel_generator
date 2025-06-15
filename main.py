bd3a6a89f4c20d3fe7fd74dcd3d669c3a9f3b083
狂拽酷炫 <nv@bestbuy.com>
2025-06-15 16:19:26 +0800

    添加 事件类型4, 刚才忘了加了

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ df['组织1'] = df['组织1'].str.strip()
 df['组织2'] = df['组织2'].str.strip()
 
 event_type_split = df['事件类型'].str.split('/', expand=True)
-for i in range(3):
+for i in range(4):
     df[f'事件类型{i+1}'] = event_type_split[i].str.strip()
 
 # 计算 Lead Time
@@ -28,7 +28,7 @@ desired_columns = [
     '任务ID', '标题', '创建者', '执行者', '紧急程度', '影响级', '优先级',
     '事件来源', '联系人', '单量', 'Jira工单',
     '组织1', '组织2',
-    '事件类型1', '事件类型2', '事件类型3',
+    '事件类型1', '事件类型2', '事件类型3','事件类型4',
     '报单时间', '完成时间', '是否完成', '借助伙伴资源',
     'Lead Time'
 ]
